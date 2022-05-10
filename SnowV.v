e22dda5bc01ee2ee8e04ff5ab3b5852ef41dc2bb
jacklc <92915352+cn-jacklc@users.noreply.github.com>
2022-05-10 14:48:06 +0800

    Update SnowV.v
    fix 2 bugs

--- SnowV.v
+++ SnowV.v
@@ -91,7 +91,7 @@ always@(posedge clk)
 always@(posedge clk)
     if(!rst_n)
         length_cnt<=64'b0;
-	else if(length_cnt==length||(length_cnt==64'd15&&initial_enable))
+	else if((valid&&length_cnt==length)||(length_cnt==64'd15&&initial_enable))
 	    length_cnt<=64'b0;
     else if(valid||initial_enable)
         length_cnt<=length_cnt+64'd1;	
@@ -148,23 +148,23 @@ endfunction
 always@(posedge clk)
     if(!rst_n)
     begin
-	    a0<=16'b0;
-		a1<=16'b0;
-		a2<=16'b0;
-		a3<=16'b0;
-		a4<=16'b0;
-		a5<=16'b0;
-		a6<=16'b0;
-		a7<=16'b0;
-		a8<=16'b0;
-		a9<=16'b0;
-		a10<=16'b0;
-		a11<=16'b0;
-	    a12<=16'b0;
+	a0<=16'b0;
+	a1<=16'b0;
+	a2<=16'b0;
+	a3<=16'b0;
+	a4<=16'b0;
+	a5<=16'b0;
+	a6<=16'b0;
+	a7<=16'b0;
+	a8<=16'b0;
+	a9<=16'b0;
+	a10<=16'b0;
+	a11<=16'b0;
+	a12<=16'b0;
         a13<=16'b0;
         a14<=16'b0;
         a15<=16'b0;
-		b0<=16'b0;
+	b0<=16'b0;
         b1<=16'b0;
         b2<=16'b0;
         b3<=16'b0;
@@ -180,147 +180,147 @@ always@(posedge clk)
         b13<=16'b0;
         b14<=16'b0;
         b15<=16'b0;
-	end
-	else if(start)
-	begin
-	    a0 <=iv[15:0];
-		a1 <=iv[31:16];
-		a2 <=iv[47:32];
-		a3 <=iv[63:48];
-		a4 <=iv[79:64];
-		a5 <=iv[95:80];
-		a6 <=iv[111:96];
-		a7 <=iv[127:112];
-		a8 <=key[15:0];
-		a9 <=key[31:16];
-		a10<=key[47:32];
-		a11<=key[63:18];
-		a12<=key[79:64];
-		a13<=key[95:80];
-		a14<=key[111:96];
-		a15<=key[127:112];
-		b0 <=16'b0;
-		b1 <=16'b0;
-		b2 <=16'b0;
-		b3 <=16'b0;
-		b4 <=16'b0;
-		b5 <=16'b0;
-		b6 <=16'b0;
-		b7 <=16'b0;
-		b8 <=key[143:128];
-		b9 <=key[159:144];
-		b10<=key[175:160];
-		b11<=key[191:176];
-		b12<=key[207:192];
-		b13<=key[223:208];
-		b14<=key[239:224];
-		b15<=key[255:240];
-	end
-	else if(initial_enable)
-	begin
-	    a0 <=a8;
-		a1 <=a9; 
-		a2 <=a10;
-		a3 <=a11;
-		a4 <=a12;
-		a5 <=a13;
-		a6 <=a14;
-		a7 <=a15;
-		a8 <=b0^mul_alpha(a0)^a1^mul_alpha_inv(a8) ^z[15:0];
-		a9 <=b1^mul_alpha(a1)^a2^mul_alpha_inv(a9) ^z[31:16];
-		a10<=b2^mul_alpha(a2)^a3^mul_alpha_inv(a10)^z[47:32];
-		a11<=b3^mul_alpha(a3)^a4^mul_alpha_inv(a11)^z[63:48];
-		a12<=b4^mul_alpha(a4)^a5^mul_alpha_inv(a12)^z[79:64];
-		a13<=b5^mul_alpha(a5)^a6^mul_alpha_inv(a13)^z[95:80];
-		a14<=b6^mul_alpha(a6)^a7^mul_alpha_inv(a14)^z[111:96];
-		a15<=b7^mul_alpha(a7)^a8^mul_alpha_inv(a15)^z[127:112];
-		b0 <=b8; 
-		b1 <=b9;
-		b2 <=b10;
-		b3 <=b11;
-		b4 <=b12;
-		b5 <=b13;
-		b6 <=b14;
-		b7 <=b15;
-		b8 <=a0^mul_beta(b0)^b3^ mul_beta_inv(b8 );
-		b9 <=a1^mul_beta(b1)^b4^ mul_beta_inv(b9 );
-		b10<=a2^mul_beta(b2)^b5^ mul_beta_inv(b10);
-		b11<=a3^mul_beta(b3)^b6^ mul_beta_inv(b11);
-		b12<=a4^mul_beta(b4)^b7^ mul_beta_inv(b12);
-		b13<=a5^mul_beta(b5)^b8^ mul_beta_inv(b13);
-		b14<=a6^mul_beta(b6)^b9^ mul_beta_inv(b14);
-		b15<=a7^mul_beta(b7)^b10^mul_beta_inv(b15);
-	end
-	else if(valid)
-	begin
-	    a0 <=a8;
-		a1 <=a9; 
-		a2 <=a10;
-		a3 <=a11;
-		a4 <=a12;
-		a5 <=a13;
-		a6 <=a14;
-		a7 <=a15;
-		a8 <=b0^mul_alpha(a0)^a1^mul_alpha_inv(a8);
-		a9 <=b1^mul_alpha(a1)^a2^mul_alpha_inv(a9);
-		a10<=b2^mul_alpha(a2)^a3^mul_alpha_inv(a10);
-		a11<=b3^mul_alpha(a3)^a4^mul_alpha_inv(a11);
-		a12<=b4^mul_alpha(a4)^a5^mul_alpha_inv(a12);
-		a13<=b5^mul_alpha(a5)^a6^mul_alpha_inv(a13);
-		a14<=b6^mul_alpha(a6)^a7^mul_alpha_inv(a14);
-		a15<=b7^mul_alpha(a7)^a8^mul_alpha_inv(a15);
-		b0 <=b8; 
-		b1 <=b9;
-		b2 <=b10;
-		b3 <=b11;
-		b4 <=b12;
-		b5 <=b13;
-		b6 <=b14;
-		b7 <=b15;
-		b8 <=a0^mul_beta(b0)^b3^ mul_beta_inv(b8 );
-		b9 <=a1^mul_beta(b1)^b4^ mul_beta_inv(b9 );
-		b10<=a2^mul_beta(b2)^b5^ mul_beta_inv(b10);
-		b11<=a3^mul_beta(b3)^b6^ mul_beta_inv(b11);
-		b12<=a4^mul_beta(b4)^b7^ mul_beta_inv(b12);
-		b13<=a5^mul_beta(b5)^b8^ mul_beta_inv(b13);
-		b14<=a6^mul_beta(b6)^b9^ mul_beta_inv(b14);
-		b15<=a7^mul_beta(b7)^b10^mul_beta_inv(b15);
-	end
-	else
-	begin
-	    a0 <=a0 ;
-	    a1 <=a1 ;
-	    a2 <=a2 ;
-	    a3 <=a3 ;
-	    a4 <=a4 ;
-	    a5 <=a5 ;
-	    a6 <=a6 ;
-	    a7 <=a7 ;
-	    a8 <=a8 ;
-	    a9 <=a9 ;
-	    a10<=a10;
-	    a11<=a11;
-	    a12<=a12;
-	    a13<=a13;
-	    a14<=a14;
-	    a15<=a15;
-	    b0 <=b0 ;
-	    b1 <=b1 ;
-	    b2 <=b2 ;
-	    b3 <=b3 ;
-	    b4 <=b4 ;
-	    b5 <=b5 ;
-	    b6 <=b6 ;
-	    b7 <=b7 ;
-	    b8 <=b8 ;
-	    b9 <=b9 ;
+    end
+    else if(start)
+    begin
+	a0 <=iv[15:0];
+	a1 <=iv[31:16];
+	a2 <=iv[47:32];
+	a3 <=iv[63:48];
+	a4 <=iv[79:64];
+	a5 <=iv[95:80];
+	a6 <=iv[111:96];
+	a7 <=iv[127:112];
+	a8 <=key[15:0];
+	a9 <=key[31:16];
+	a10<=key[47:32];
+	a11<=key[63:48];
+	a12<=key[79:64];
+	a13<=key[95:80];
+	a14<=key[111:96];
+	a15<=key[127:112];
+	b0 <=16'b0;
+	b1 <=16'b0;
+	b2 <=16'b0;
+	b3 <=16'b0;
+	b4 <=16'b0;
+	b5 <=16'b0;
+	b6 <=16'b0;
+	b7 <=16'b0;
+	b8 <=key[143:128];
+	b9 <=key[159:144];
+	b10<=key[175:160];
+	b11<=key[191:176];
+	b12<=key[207:192];
+	b13<=key[223:208];
+	b14<=key[239:224];
+	b15<=key[255:240];
+    end
+    else if(initial_enable)
+    begin
+        a0 <=a8;
+        a1 <=a9; 
+        a2 <=a10;
+        a3 <=a11;
+        a4 <=a12;
+        a5 <=a13;
+        a6 <=a14;
+        a7 <=a15;
+        a8 <=b0^mul_alpha(a0)^a1^mul_alpha_inv(a8) ^z[15:0];
+        a9 <=b1^mul_alpha(a1)^a2^mul_alpha_inv(a9) ^z[31:16];
+        a10<=b2^mul_alpha(a2)^a3^mul_alpha_inv(a10)^z[47:32];
+        a11<=b3^mul_alpha(a3)^a4^mul_alpha_inv(a11)^z[63:48];
+        a12<=b4^mul_alpha(a4)^a5^mul_alpha_inv(a12)^z[79:64];
+        a13<=b5^mul_alpha(a5)^a6^mul_alpha_inv(a13)^z[95:80];
+        a14<=b6^mul_alpha(a6)^a7^mul_alpha_inv(a14)^z[111:96];
+        a15<=b7^mul_alpha(a7)^a8^mul_alpha_inv(a15)^z[127:112];
+        b0 <=b8; 
+        b1 <=b9;
+        b2 <=b10;
+        b3 <=b11;
+        b4 <=b12;
+        b5 <=b13;
+        b6 <=b14;
+        b7 <=b15;
+        b8 <=a0^mul_beta(b0)^b3^ mul_beta_inv(b8 );
+        b9 <=a1^mul_beta(b1)^b4^ mul_beta_inv(b9 );
+        b10<=a2^mul_beta(b2)^b5^ mul_beta_inv(b10);
+        b11<=a3^mul_beta(b3)^b6^ mul_beta_inv(b11);
+        b12<=a4^mul_beta(b4)^b7^ mul_beta_inv(b12);
+        b13<=a5^mul_beta(b5)^b8^ mul_beta_inv(b13);
+        b14<=a6^mul_beta(b6)^b9^ mul_beta_inv(b14);
+        b15<=a7^mul_beta(b7)^b10^mul_beta_inv(b15);
+    end
+    else if(valid)
+    begin
+	a0 <=a8;
+	a1 <=a9; 
+	a2 <=a10;
+	a3 <=a11;
+	a4 <=a12;
+	a5 <=a13;
+	a6 <=a14;
+	a7 <=a15;
+	a8 <=b0^mul_alpha(a0)^a1^mul_alpha_inv(a8);
+	a9 <=b1^mul_alpha(a1)^a2^mul_alpha_inv(a9);
+	a10<=b2^mul_alpha(a2)^a3^mul_alpha_inv(a10);
+	a11<=b3^mul_alpha(a3)^a4^mul_alpha_inv(a11);
+	a12<=b4^mul_alpha(a4)^a5^mul_alpha_inv(a12);
+	a13<=b5^mul_alpha(a5)^a6^mul_alpha_inv(a13);
+	a14<=b6^mul_alpha(a6)^a7^mul_alpha_inv(a14);
+	a15<=b7^mul_alpha(a7)^a8^mul_alpha_inv(a15);
+	b0 <=b8; 
+	b1 <=b9;
+	b2 <=b10;
+	b3 <=b11;
+	b4 <=b12;
+	b5 <=b13;
+	b6 <=b14;
+	b7 <=b15;
+	b8 <=a0^mul_beta(b0)^b3^ mul_beta_inv(b8 );
+	b9 <=a1^mul_beta(b1)^b4^ mul_beta_inv(b9 );
+	b10<=a2^mul_beta(b2)^b5^ mul_beta_inv(b10);
+	b11<=a3^mul_beta(b3)^b6^ mul_beta_inv(b11);
+	b12<=a4^mul_beta(b4)^b7^ mul_beta_inv(b12);
+	b13<=a5^mul_beta(b5)^b8^ mul_beta_inv(b13);
+	b14<=a6^mul_beta(b6)^b9^ mul_beta_inv(b14);
+	b15<=a7^mul_beta(b7)^b10^mul_beta_inv(b15);
+    end
+    else
+    begin
+	a0 <=a0 ;
+	a1 <=a1 ;
+	a2 <=a2 ;
+	a3 <=a3 ;
+	a4 <=a4 ;
+	a5 <=a5 ;
+	a6 <=a6 ;
+	a7 <=a7 ;
+	a8 <=a8 ;
+	a9 <=a9 ;
+	a10<=a10;
+	a11<=a11;
+	a12<=a12;
+	a13<=a13;
+	a14<=a14;
+	a15<=a15;
+	b0 <=b0 ;
+	b1 <=b1 ;
+	b2 <=b2 ;
+	b3 <=b3 ;
+	b4 <=b4 ;
+	b5 <=b5 ;
+	b6 <=b6 ;
+	b7 <=b7 ;
+	b8 <=b8 ;
+	b9 <=b9 ;
         b10<=b10;
         b11<=b11;
         b12<=b12;
         b13<=b13;
         b14<=b14;
-		b15<=b15;
-	end               //need icg
+	b15<=b15;
+    end               //need icg
 	
 	
 //fsm part
